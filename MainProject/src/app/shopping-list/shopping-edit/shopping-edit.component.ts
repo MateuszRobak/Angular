7445c37b893763fbe6cab37de3641abd2d0a3f0f
MateuszRobak <mateusz.robak90@gmail.com>
2019-08-20 15:45:30 +0200

    Added items to shopping list

--- MainProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ MainProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css','../../app.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  numer: number = 0;
+  
+  funkcja(){
+    return this.numer * 2;  
+  }
 
   constructor() { }
 
